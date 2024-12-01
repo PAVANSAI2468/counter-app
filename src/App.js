fb51ec61d581307ec5dd4b5e5f409d2c35406407
Pavan Sai <123661617+PAVANSAI2468@users.noreply.github.com>
2024-12-01 15:43:49 +0000

    Final code

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ export default function App() {
   const [a, setA] = useState(0);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Counter App</h1>
       <button onClick={() => setA(a + 1)}>INC</button>
       <p>{a}</p>
       <button disabled={a <= 0} onClick={() => setA(a - 1)}>
